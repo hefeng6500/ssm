e5f11b739b80dc2f77cba1d20d9d9820837ac0de
hefeng6500 <2443992009@qq.com>
2022-12-31 21:56:22 +0800

    feat: 完成页面增删改查功能

--- src/main/java/com/yang/config/SpringMvcConfig.java
+++ src/main/java/com/yang/config/SpringMvcConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
-@ComponentScan("com.yang.controller")
+@ComponentScan({"com.yang.controller", "com.yang.config"})
 @EnableWebMvc
 public class SpringMvcConfig {
 }
